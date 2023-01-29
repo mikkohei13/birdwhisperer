7a98a1a5ed4be1ecbd96656abb96abdef1a445a8
Mikko Heikkinen <hawk@biomi.org>
2023-01-29 17:56:15 +0200

    Enter filename as a param, print data as python variables

--- app/analyze.py
+++ app/analyze.py
@@ -1,11 +1,24 @@
 
-import whisper
 import time
+import sys
+import os
+
+filename_param = sys.argv[1]
 
 print("-----\n")
 
+filename = "./audio/" + filename_param
+
+import os
+
+if os.path.isfile(filename):
+    print("The file exists.")
+else:
+    exit(f"The file {filename} does not exist.")
+
+import whisper
 model = "large"
-filename = "./audio/" + "maaliskuu1998.mp3"
+#model = "small" # debug
 print(f"{model}, {filename}")
 
 start = time.time()
@@ -13,10 +26,16 @@ print("start at " + str(start))
 
 model = whisper.load_model(model)
 result = model.transcribe(filename, fp16=False, language="Finnish")
-print(result["text"])
-
-time.sleep(1)
+#print(result["text"])
+#time.sleep(1)
 
 end = time.time()
 print("end at " + str(end))
 print("elapsed " + str(end - start))
+
+print("-----\n")
+
+print(f"transcript = \"{result['text']}\"")
+print(f"filename = \"{filename_param}\"")
+
+print("-----\n")
